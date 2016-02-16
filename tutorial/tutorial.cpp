e25eb3a0cdb65f20ea6753b9be252b01bdcffb16
Olivier Goffart <ogoffart@woboq.com>
2016-02-16 18:08:08 +0100

    More on the tutorial

--- tutorial/tutorial.cpp
+++ tutorial/tutorial.cpp
@@ -33,7 +33,8 @@
  */
 
 
-/**************************************************************************************************/
+/** ******************************************************************************************** **/
+/** INTRODUCTION **/
 
 // In a header file you would include the wobjectdefs.h header
 #include <wobjectdefs.h>
@@ -96,6 +97,154 @@ void aaa(MyObject *obj1) {
     ok = ok && QObject::connect(obj1, SIGNAL(mySignal(QString)), obj1, SLOT(mySlot(QString)));
     Q_ASSERT(ok);
 }
+
+
+/** ******************************************************************************************** **/
+/** SLOTS **/
+class SlotTutorial : public QObject {
+    W_OBJECT(SlotTutorial)
+
+    /**
+
+       W_SLOT( <slot name> [, (<parametters types>) ]  [, <flags>]* )
+
+       The W_SLOT macro needs to be put after the slot declaration.
+       The W_SLOT macro can have flags:
+       - Specifying the the access:  W_Access::Protected, W_Access::Private
+         or W_Access::Public (the default)
+       - W_Compat: for deprecated methods (equivalent of Q_MOC_COMPAT
+
+       The W_SLOT macro can optionally have a list of parametter types as second
+       argument to disambiguate or declarre types.
+     */
+
+    /* Examples: */
+protected:
+    // declares a protected slot
+    void protectedSlot() {}
+    W_SLOT(protectedSlot, W_Access::Protected)
+private:
+    // and a private slot
+    void privateSlot() {}
+    W_SLOT(protectedSlot, W_Access::Private)
+
+public:
+    // overloaded function needs a parametter list as second argument of the macro
+    // to disambiguate
+    void overload() {}
+    W_SLOT(overload, ())
+
+    void overload(int) {}
+    W_SLOT(overload, (int))
+private:
+    void overload(double) {}
+    W_SLOT(overload, (double), W_Access::Private)
+    void overload(int, int) {}
+    W_SLOT(overload, (int, int), W_Access::Private)
+};
+
+W_OBJECT_IMPL(SlotTutorial)
+
+
+/** ******************************************************************************************** **/
+/** SIGNALS **/
+
+class SignalTutorial : public QObject {
+    W_OBJECT(SignalTutorial)
+
+    /**
+
+       W_SIGNAL_1( <signal signature> )
+       W_SIGNAL_2( <signal name> [, (<parametters types>) ] , <parametters> )
+
+       Almost Exactly like CS_SIGNAL, but
+        - It is not possible to specify anything else than public signals
+        - CS_SIGNAL_OVERLOAD is merged with W_SIGNAL_2
+     */
+
+public:
+    // example:
+    W_SIGNAL_1(void sig1(int a , int b))
+    W_SIGNAL_2(sig1, a, b)
+
+    // technicly the W_SIGNAL_1 is not nessesary. I only have it for symetry with copperspice but
+    // I guess i should get rid of it.  But W_SIGNAL_2 should always come after the declaration
+    // without semi colon at the end of the declaration:
+    // So another way to declare a signal:
+    void sig2(int a, int b) W_SIGNAL_2(sig2, a, b)
+
+    // For overloaded signals:
+    W_SIGNAL_1(void overload(int a, int b))
+    W_SIGNAL_2(overload, (int, int), a, b)
+};
+
+W_OBJECT_IMPL(SignalTutorial)
+
+
+/** ******************************************************************************************** **/
+/** Gadgets,  invokables, constructor **/
+
+
+class InvokableTutorial  {
+    // Well, just like Qt has Q_GADGET, here we have W_GADGET
+    W_GADGET(InvokableTutorial)
+
+public:
+    /** W_INVOKABLE is the same as W_SLOT.
+     * It can take another flag (W_Scriptable) which corresponds to Q_SCRIPTABLE*/
+
+    void myInvokable() {}
+    W_INVOKABLE(myInvokable)
+
+
+    /** W_CONSTRUCTOR(<parametter types>)
+        for Q_INVOKABLE constructor, just pass the parametter types to this macro.
+        one can have W_CONSTRUCTOR() for the default constructor even if it is implicit
+     */
+
+
+    InvokableTutorial(int, int) {}
+    W_CONSTRUCTOR(int,  int)
+
+    InvokableTutorial(void*, void* =nullptr) {}
+    W_CONSTRUCTOR(void*, void*)
+    // because of tehe default argument we can also do that:  (only in this macro)
+    W_CONSTRUCTOR(void*)
+};
+
+// For gadget there is also a different IMPL macro
+W_GADGET_IMPL(InvokableTutorial)
+
+
+/** ******************************************************************************************** **/
+/** Enums **/
+
+
+class EnumTutorial  {
+    W_GADGET(EnumTutorial)
+
+public:
+    /** W_ENUM(<name>, <values>)
+      Similar to Q_ENUM, but we also have to manually write all the values.
+      Maybe in the future it could be made nicer that declares the enum in the same macro
+      but now that's all we have */
+    enum MyEnum { Blue, Red, Green, Yellow = 45, Violet = Blue + Green*3 };
+
+    W_ENUM(MyEnum, Blue, Red, Green, Yellow)
+
+    // CS_ENUM is a bit better, but i don't believe this works with complex expression like
+    // "Blue + Green*3".
+
+    // W_ENUM is currently limited to 16 enum values.
+    // enum class are not yet supported
+
+    // There is a W_FLAG  which is the same as Q_FLAG
+};
+
+W_GADGET_IMPL(EnumTutorial)
+
+
+
 
 
 
