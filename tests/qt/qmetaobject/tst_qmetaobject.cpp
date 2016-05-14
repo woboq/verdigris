610f946df3a50bebe4a93fd426db819f1ee0f360
Olivier Goffart <ogoffart@woboq.com>
2016-05-14 11:19:58 +0200

    tst_qmetaobject:  update to v5.7.0-beta1

--- tests/qt/qmetaobject/tst_qmetaobject.cpp
+++ tests/qt/qmetaobject/tst_qmetaobject.cpp
@@ -1,31 +1,26 @@
 /****************************************************************************
 **
-** Copyright (C) 2015 The Qt Company Ltd.
-** Contact: http://www.qt.io/licensing/
+** Copyright (C) 2016 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of the test suite of the Qt Toolkit.
 **
-** $QT_BEGIN_LICENSE:LGPL21$
+** $QT_BEGIN_LICENSE:GPL-EXCEPT$
 ** Commercial License Usage
 ** Licensees holding valid commercial Qt licenses may use this file in
 ** accordance with the commercial license agreement provided with the
 ** Software or, alternatively, in accordance with the terms contained in
 ** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see http://www.qt.io/terms-conditions. For further
-** information use the contact form at http://www.qt.io/contact-us.
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
 **
-** GNU Lesser General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU Lesser
-** General Public License version 2.1 or version 3 as published by the Free
-** Software Foundation and appearing in the file LICENSE.LGPLv21 and
-** LICENSE.LGPLv3 included in the packaging of this file. Please review the
-** following information to ensure the GNU Lesser General Public License
-** requirements will be met: https://www.gnu.org/licenses/lgpl.html and
-** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-** As a special exception, The Qt Company gives you certain additional
-** rights. These rights are described in The Qt Company LGPL Exception
-** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
 **
 ** $QT_END_LICENSE$
 **
@@ -147,9 +142,28 @@ namespace MyNamespace {
         W_PROPERTY(MyFlags, myFlags READ myFlags WRITE setMyFlags)
 
     };
+
+    // Test inherits
+    class MyClassSubclass : public MyClass
+    {
+        W_OBJECT(MyClassSubclass)
+    };
+
+    class MyClassSubclass2 : public MyClass2
+    {
+        W_OBJECT(MyClassSubclass2)
+    };
+
+    class MyClass2Subclass : public MyClass
+    {
+        W_OBJECT(MyClass2Subclass)
+    };
 }
 W_OBJECT_IMPL(MyNamespace::MyClass)
 W_OBJECT_IMPL(MyNamespace::MyClass2)
+W_OBJECT_IMPL(MyNamespace::MyClassSubclass)
+W_OBJECT_IMPL(MyNamespace::MyClassSubclass2)
+W_OBJECT_IMPL(MyNamespace::MyClass2Subclass)
 
 class tst_QMetaObject : public QObject
 {
@@ -228,6 +242,10 @@ private slots:
     void signalIndex_data();
     void signalIndex();
     void enumDebugStream();
+
+    void inherits_data();
+    void inherits();
+
     /***/
     W_SLOT(connectSlotsByName, W_Access::Private)
     W_SLOT(invokeMetaMember, W_Access::Private)
@@ -269,6 +287,8 @@ private slots:
     W_SLOT(signalIndex, W_Access::Private)
     W_SLOT(enumDebugStream, W_Access::Private)
 
+    W_SLOT(inherits_data, W_Access::Private)
+    W_SLOT(inherits, W_Access::Private)
 
 signals:
     void value6Changed()
@@ -1543,6 +1563,37 @@ void tst_QMetaObject::enumDebugStream()
     MyNamespace::MyClass::MyFlags f2 = MyNamespace::MyClass::MyFlag2 | MyNamespace::MyClass::MyFlag3;
     QTest::ignoreMessage(QtDebugMsg, "QFlags<MyNamespace::MyClass::MyFlags>(MyFlag1) QFlags<MyNamespace::MyClass::MyFlags>(MyFlag2|MyFlag3)");
     qDebug() << f1 << f2;
+}
+
+void tst_QMetaObject::inherits_data()
+{
+    QTest::addColumn<const QMetaObject *>("derivedMetaObject");
+    QTest::addColumn<const QMetaObject *>("baseMetaObject");
+    QTest::addColumn<bool>("inheritsResult");
+
+    QTest::newRow("MyClass inherits QObject")
+        << &MyNamespace::MyClass::staticMetaObject << &QObject::staticMetaObject << true;
+    QTest::newRow("QObject inherits MyClass")
+        << &QObject::staticMetaObject << &MyNamespace::MyClass::staticMetaObject << false;
+    QTest::newRow("MyClass inherits MyClass")
+        << &MyNamespace::MyClass::staticMetaObject << &MyNamespace::MyClass::staticMetaObject << true;
+    QTest::newRow("MyClassSubclass inherits QObject")
+        << &MyNamespace::MyClassSubclass::staticMetaObject << &QObject::staticMetaObject << true;
+    QTest::newRow("MyClassSubclass2 inherits QObject")
+        << &MyNamespace::MyClassSubclass2::staticMetaObject << &QObject::staticMetaObject << true;
+    QTest::newRow("MyClassSubclass2 inherits MyClass2")
+        << &MyNamespace::MyClassSubclass2::staticMetaObject << &MyNamespace::MyClass2Subclass::staticMetaObject << false;
+}
+
+void tst_QMetaObject::inherits()
+{
+#if QT_VERSION >= QT_VERSION_CHECK(5, 7, 0)
+    QFETCH(const QMetaObject *, derivedMetaObject);
+    QFETCH(const QMetaObject *, baseMetaObject);
+    QFETCH(bool, inheritsResult);
+
+    QCOMPARE(derivedMetaObject->inherits(baseMetaObject), inheritsResult);
+#endif
 }
 
 QTEST_MAIN(tst_QMetaObject)
