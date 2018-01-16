73c6df4ada819cd7369b6f9e893afb690a079353
Olivier Goffart <ogoffart@woboq.com>
2018-01-16 12:47:02 +0100

    Update header of main source files

--- src/wobjectdefs.h
+++ src/wobjectdefs.h
@@ -1,7 +1,10 @@
 /****************************************************************************
- *  Copyright (C) 2013-2015 Woboq GmbH
- *  Olivier Goffart <contact at woboq.com>
+ *  Copyright (C) 2016-2018 Woboq GmbH
+ *  Olivier Goffart <ogoffart at woboq.com>
  *  https://woboq.com/
+ *
+ *  This file is part of Verdigris: a way to use Qt without moc.
+ *  https://github.com/woboq/verdigris
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as
