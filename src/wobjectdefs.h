35342ca5c785a48cb36e4cfc6898dc4d206808d4
Olivier Goffart <ogoffart@woboq.com>
2018-02-12 13:49:38 +0100

    Define a version number
    https://github.com/woboq/verdigris/issues/17

--- src/wobjectdefs.h
+++ src/wobjectdefs.h
@@ -32,6 +32,8 @@
 #error Verdigris requires C++14 relaxed constexpr
 #endif
 
+#define W_VERSION 0x010000
+
 namespace w_internal {
 using std::index_sequence;  // From C++14, make sure to enable the C++14 option in your compiler
 
