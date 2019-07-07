f2f24cabc85f42fde1a65fdc9601aac86b112f20
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2019-07-07 17:21:24 +0200

    added more documentation and moved MetaMethodInfoBuilder to w_internal

--- src/wobjectcpp.h
+++ src/wobjectcpp.h
@@ -1,28 +1,15 @@
 #pragma once
 #include "wobjectdefs.h"
 
-#if __cplusplus <= 201700L
 namespace w_internal {
-
+#if __cplusplus <= 201700L
 template<bool... Bs>
 constexpr bool all() {
     bool b = true;
     ordered2<bool>({(b = b && Bs)...});
     return b;
 }
-
-} // namespace w_internal
 #endif
-
-namespace w_cpp {
-
-using w_internal::StringView;
-using w_internal::viewLiteral;
-
-template<typename T>
-constexpr auto makeProperty(StringView name, StringView type) {
-    return w_internal::MetaPropertyInfo<T>{ name, type, {}, {}, {}, {}, {} };
-}
 
 template<class F, int Flags, class IC = int, class ParamTypes = w_internal::StringViewArray<>, class ParamNames = w_internal::StringViewArray<>>
 struct MetaMethodInfoBuilder {
@@ -59,22 +46,69 @@ struct MetaMethodInfoBuilder {
         -> MetaMethodInfoBuilder<F, w_internal::summed<fs ..., Flags>, IC, ParamTypes, ParamNames> {
 #endif
             return {name, func, paramTypes, paramNames};
-}
-template<class IC2>
-constexpr auto setIntegralConstant() const -> MetaMethodInfoBuilder<F, Flags, IC2, ParamTypes, ParamNames> {
-    return {name, func, paramTypes, paramNames};
-}
+    }
+    template<class IC2>
+    constexpr auto setIntegralConstant() const -> MetaMethodInfoBuilder<F, Flags, IC2, ParamTypes, ParamNames> {
+        return {name, func, paramTypes, paramNames};
+    }
 
-constexpr auto build() const -> w_internal::MetaMethodInfo<F, Flags, IC, ParamTypes, ParamNames> {
-    return {func, name, paramTypes, paramNames};
-}
+    constexpr auto build() const -> w_internal::MetaMethodInfo<F, Flags, IC, ParamTypes, ParamNames> {
+        return {func, name, paramTypes, paramNames};
+    }
 };
 
-template<typename F>
-constexpr auto makeSignalBuilder(StringView name, F func) {
-    return MetaMethodInfoBuilder<F, W_MethodType::Signal.value>{name, func};
+} // namespace w_internal
+
+namespace w_cpp {
+
+/// Very simple constexpr version of std::string_view
+///
+/// example usage:
+///     constexpr char text[6] = "Hello"; // if you have a string literal use `viewLiteral` below.
+///     constexpr auto view = w_cpp::StringView{&text[0], &text[6]};
+///
+/// \note the end pointer has to point behind the \0
+using w_internal::StringView;
+
+/// Generate a constexpr StringView from a C++ string literal.
+///
+/// example usage:
+///     constexpr auto view & w_cpp::StringView{"Hello"};
+using w_internal::viewLiteral;
+
+/// create a compile time property description for registraton usage
+///
+/// \arg T the type of the property
+/// \arg name the compile time StringView for the property name
+/// \arg type the compile time StringView for the type of the property
+///
+/// Following methods may be called in chain:
+/// * .setGetter(F) - set the member function that returns the value of the property `() -> T`
+/// * .setSetter(F) - set the member function that changes the value of the property `(T) -> void`
+/// * .setMember(M) - set the class attribute to read and write the value of the property
+/// * .setNotify(F) - set the member function that represents the changed signal for the property
+/// * .setReset(F) - set the member function that resets the property value to it's default
+/// * .addFlag<F>() - add one or multiple flags for the property
+template<typename T>
+constexpr auto makeProperty(StringView name, StringView type) {
+    return w_internal::MetaPropertyInfo<T>{ name, type, {}, {}, {}, {}, {} };
 }
 
+/// create a builder for a signal description
+///
+/// \arg name the compile time StringView for the function name
+/// \arg func the member function that implements the signal
+///
+/// Following methods may be call in chain:
+/// * .setParamNames(StringView...) - set the names for all the function parameters (optional)
+/// * .setParamTypes(StringView...) - set the names of for all parameter types
+/// * .addFlag(Flags...) - add some methods flags
+/// * .setIntegralConstant<T>() - set a compile time integral value type as a unique identifier
+/// * .build() - build the final MethodInfo for this signal
+template<typename F>
+constexpr auto makeSignalBuilder(StringView name, F func) {
+    return w_internal::MetaMethodInfoBuilder<F, W_MethodType::Signal.value>{name, func};
+}
 
 } // namespace w_cpp
 
