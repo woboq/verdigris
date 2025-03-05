9b3e3f26251444bf48011202db85c892b0f45c57
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-03-05 04:58:35 -0500

    core: update qTryMetaTypeInterfaceForType for Qt 6.9

--- src/wobjectimpl.h
+++ src/wobjectimpl.h
@@ -793,13 +793,20 @@ struct DataBuilder {
         intCount += sizeof... (Ts);
     }
 
-#if QT_VERSION >= QT_VERSION_CHECK(6,0,0)
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+#if QT_VERSION >= QT_VERSION_CHECK(6, 9, 0)
+    template<class Unique, bool TypeMustBeComplete>
+    static constexpr const QtPrivate::QMetaTypeInterface *metaTypeInterface = QtPrivate::qTryMetaTypeInterfaceForType<Unique,  std::conditional_t<TypeMustBeComplete, void, Unique>>();
+#else
     template<class T, bool isComplete>
-    static constexpr const QtPrivate::QMetaTypeInterface *metaTypeInterface = QtPrivate::qTryMetaTypeInterfaceForType<T, QtPrivate::TypeAndForceComplete<T, std::integral_constant<bool, isComplete>>>();
-
-    template<class T, bool isComplete = false>
+    static constexpr const QtPrivate::QMetaTypeInterface *metaTypeInterface
+        = QtPrivate::qTryMetaTypeInterfaceForType<
+            T,
+            QtPrivate::TypeAndForceComplete<T, std::integral_constant<bool, isComplete>>>();
+#endif
+    template<class T, bool TypeMustBeComplete = false>
     constexpr void addMetaType() {
-        *metaTypeP++ = metaTypeInterface<T, isComplete>;
+        *metaTypeP++ = metaTypeInterface<T, TypeMustBeComplete>;
         metaTypeCount += 1;
     }
 #endif
